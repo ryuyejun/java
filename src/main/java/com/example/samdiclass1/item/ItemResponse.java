972589b76c995ec0b5b9a77bb15167bbb6b85e14
ryuyejun <yjryu@dgsw.hs.kr>
2026-05-13 20:15:29 +0900

    feat:아이템조회

---
+++ src/main/java/com/example/samdiclass1/item/ItemResponse.java
@@ -0,0 +1,20 @@
+package com.example.samdiclass1.item;
+
+public record ItemResponse (
+        String name,
+        String type,
+        int attackPower,
+        int durability,
+        int enhanceLevel
+){
+    public static ItemResponse of(Item item){
+        return new ItemResponse(
+                item.getName(),
+                item.getType(),
+                item.getAttackPower(),
+                item.getDurability(),
+                item.getEnchancedLevel()
+
+        );
+    }
+}
